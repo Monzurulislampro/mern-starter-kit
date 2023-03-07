9e6fc185333715197f55a8d7b1fd4dcbba3a54d6
Monzurul Islam <mounzrul@gmail.com>
2023-03-07 22:44:34 +0600

    formatting change

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,6 @@ const mongoose = require("mongoose");
 app.use(cors());
 app.use(express.json());
 const uri = process.env.URI;
-console.log(uri);
 mongoose
   .connect(uri, {
     useNewUrlParser: true,
@@ -21,5 +20,33 @@ mongoose
   .catch((error) => {
     console.log("Database connection error: " + error);
   });
+const sch = {
+  name: String,
+  email: String,
+  id: Number,
+  jobStatus: String,
+};
+const monmodel = mongoose.model("NEWCOL", sch);
+app.post("/post", async (req, res) => {
+  console.log("inside post function");
+  const data = new monmodel({
+    name: req.body.name,
+    email: req.body.email,
+    id: req.body.id,
+    jobStatus: req.body.job,
+  });
+  const value = await data.save();
+  res.send(data);
+  console.log(data);
+});
+app.get("/post", function (req, res) {
+  let posts = monmodel.find({}, function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(posts);
+    }
+  });
+});
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
